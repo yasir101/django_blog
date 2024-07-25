e9d5eb5705aa0c6d8b7461475544ed0e06bc9367
Yasir Amjad <iamyasiramjad@gmail.com>
2024-07-25 13:03:40 +0500

    blog app

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts })
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
+    
